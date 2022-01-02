3683f62e4bba65b92c00f34c93fd2a79f651866e
Lennon Milicic Costa <lennon.costa@ext.hdi.com.br>
2022-01-02 19:40:59 -0300

    att

--- apps/nativescript-didremember-app/src/pages/category/category.component.ts
+++ apps/nativescript-didremember-app/src/pages/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TextField } from '@nativescript/core';
 import { Subscription } from 'rxjs';
 import { IPostit } from '../../shared/interfaces/postit.interface';
 import { PostitServiceContract } from '../../shared/services/postit/postit.service.contract';
@@ -15,10 +16,11 @@ export class CategoryComponent implements OnInit {
   selectPostit: IPostit = {} as IPostit;
   postits: IPostit[] = [];
   postits$: Subscription;
+  tfName: TextField = null;
 
   categoryForm: FormGroup = this.fb.group({
     id: ['', []],
-    name: ['', [Validators.required, Validators.minLength(2)]],
+    name: ['', [Validators.required, Validators.minLength(4)]],
     count: ['', []],
   });
 
@@ -27,7 +29,7 @@ export class CategoryComponent implements OnInit {
     private postitService: PostitServiceContract
   ) {}
 
-  get f() {
+  get fc() {
     return this.categoryForm.controls;
   }
 
@@ -43,6 +45,10 @@ export class CategoryComponent implements OnInit {
     });
   }
 
+  nameProps(ev: TextField) {
+    this.tfName = ev;
+  }
+
   cancel = (): void => {
     this.selectPostit = {} as IPostit;
     this.categoryForm.reset();
@@ -51,7 +57,7 @@ export class CategoryComponent implements OnInit {
   save = (): void => {
     if (!this.categoryForm.valid) return;
     const postit: IPostit = {
-      name: this.categoryForm.controls.name.value,
+      name: this.fc.name.value,
       id: Utils.getUniqueId(2),
       count: 0,
     };
@@ -62,9 +68,9 @@ export class CategoryComponent implements OnInit {
   edit = (): void => {
     if (!this.categoryForm.valid) return;
     const postit: IPostit = {
-      name: this.categoryForm.controls.name.value,
-      id: this.categoryForm.controls.id.value,
-      count: this.categoryForm.controls.count.value,
+      name: this.fc.name.value,
+      id: this.fc.id.value,
+      count: this.fc.count.value,
     };
     this.postitService.edit(postit);
     this.categoryForm.reset();
--- apps/nativescript-didremember-app/src/shared/design/did-input-text/did-input-text.component.ts
+++ apps/nativescript-didremember-app/src/shared/design/did-input-text/did-input-text.component.ts
@@ -1,5 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { ControlContainer, FormGroupDirective, ValidationErrors } from '@angular/forms';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  ControlContainer,
+  FormGroupDirective,
+  ValidationErrors,
+} from '@angular/forms';
+import { TextField } from '@nativescript/core';
 
 @Component({
   moduleId: module.id,
@@ -14,25 +27,45 @@ import { ControlContainer, FormGroupDirective, ValidationErrors } from '@angular
   template: `
     <StackLayout class="input-text">
       <Label class="label" [text]="label"></Label>
-      <TextField [formControlName]="controlName" [hint]="hint"></TextField>
+      <TextField
+        #elRef
+        [formControlName]="controlName"
+        [hint]="hint"
+      ></TextField>
       <StackLayout class="errors" *ngIf="dirty">
         <Label *ngIf="error?.required" text="Necessário ter um Nome"></Label>
-        <Label *ngIf="error?.minlength" text="Necessário ter {{ error?.minlength?.requiredLength }} ou mais caracteres"></Label>
+        <Label
+          *ngIf="error?.minlength"
+          text="Necessário ter {{
+            error?.minlength?.requiredLength
+          }} ou mais caracteres"
+        ></Label>
       </StackLayout>
     </StackLayout>
   `,
 })
-export class DidInputTextComponent {
+export class DidInputTextComponent implements AfterViewChecked {
+  @ViewChild('elRef') elRef: ElementRef;
+
+  @Output('props') props = new EventEmitter<TextField>();
+
   @Input('label') label = 'Label';
   @Input('hint') hint = '';
   @Input('controlName') controlName = '';
   @Input('dirty') dirty = false;
+  @Input('focus') focus = false;
   @Input('errors') set errors(valueErrors: ValidationErrors) {
     this.error = valueErrors;
   }
 
   error: ValidationErrors = {
     required: false,
-    minlength: null
+    minlength: null,
+  };
+
+  ngAfterViewChecked(): void {
+    const elTF = this.elRef.nativeElement as TextField;
+    this.props.emit(elTF);
   }
+
 }
