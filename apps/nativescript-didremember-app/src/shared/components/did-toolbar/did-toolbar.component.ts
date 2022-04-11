aa4050a65e5e689ba0c528b580f8cfae350b70d1
Lennon Milicic Costa <lennon.costa@ext.hdi.com.br>
2022-04-10 22:37:44 -0300

    ajuste header text size

--- apps/nativescript-didremember-app/src/shared/components/did-toolbar/did-toolbar.component.ts
+++ apps/nativescript-didremember-app/src/shared/components/did-toolbar/did-toolbar.component.ts
@@ -10,7 +10,7 @@ import { PostitServiceContract } from '../../services/postit/postit.service.cont
   styleUrls: ['./did-toolbar.component.scss'],
   template: `
     <StackLayout class="toolbar">
-      <GridLayout class="actions-bar" columns="auto, *, auto" rows="38">
+      <GridLayout class="actions-bar" columns="auto, *, auto" rows="auto">
         <Button *ngIf="canBack" row="0" col="0" (tap)="back()" class="icon fas" text="&#xf060;"></Button>
         <Button *ngIf="!canBack" row="0" col="0" (tap)="onDrawerButtonTap()" class="icon fas" text="&#xf0c9;"></Button>
         <Label row="0" col="2" class="title" (tap)="clear()" [text]="title"></Label>
