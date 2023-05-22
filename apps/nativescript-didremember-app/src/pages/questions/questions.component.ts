b85054a9701244692f3bc2de10c2f470725c1654
Lennon Milicic Costa <lennon.costa@ext.hdi.com.br>
2023-05-21 22:30:36 -0300

    ajustes

--- apps/nativescript-didremember-app/src/pages/questions/questions.component.ts
+++ apps/nativescript-didremember-app/src/pages/questions/questions.component.ts
@@ -1,59 +1,24 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Screen } from '@nativescript/core';
+import { Component, OnInit } from '@angular/core';
 import { IPostit } from '../../shared/interfaces/postit.interface';
-import { RouterExtensions } from '@nativescript/angular';
-import { Subscription } from 'rxjs';
 import { PostitServiceContract } from '../../shared/services/postit/postit.service.contract';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ns-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.scss'],
 })
-export class QuestionsComponent implements OnInit, OnDestroy {
-  widthDIPs = Screen.mainScreen.widthDIPs;
-  sizeScreen = 0;
-  postits: IPostit[] = [];
+export class QuestionsComponent implements OnInit {
+
   postit: IPostit = {} as IPostit;
-  postits$: Subscription;
 
   constructor(
     private postitService: PostitServiceContract,
-    private routerExtensions: RouterExtensions
-  ) {
-    this.sizeScreen = this.widthDIPs / 2 - 24;
-  }
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.postits$ = this.postitService.postits.subscribe({
-      next: (res) => {
-        this.postits = res;
-      },
-      error: (error) => {
-        console.error('categorys', error);
-        this.postits = [];
-      }
-    });
+    const id = this.activatedRoute.snapshot.params["id"];
+    this.postit = this.postitService.find(id);
   }
-
-  ngOnDestroy(): void {
-    this.postits$.unsubscribe();
-  }
-
-  onClickItem(postit: IPostit): void {
-    this.postit = this.postitService.find(postit.id);
-    if (this.postit.id) {
-      this.navigateToDetail(postit);
-    }
-  }
-
-  navigateToRegisterCategory(): void {
-    this.routerExtensions.navigate(['/category']);
-  }
-
-  navigateToDetail(item: IPostit): void {
-    this.routerExtensions.navigate(['/category', item.id]);
-  }
-
-  postitTrackBy = (postit: IPostit): string => postit.id;
 }
